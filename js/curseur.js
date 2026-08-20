44aba84052a52d2a351ebf0466be3f778beb9127
SwayneLP <pernet.lucas39@gmail.com>
2026-08-20 11:35:41 +0200

    Changing everything

--- js/curseur.js
+++
@@ -1,9 +0,0 @@
-// Au chargement du site
-window.addEventListener("load", () => {
-  document.body.classList.add("no-scroll");
-
-  // Déblocage après 4 secondes
-  setTimeout(() => {
-    document.body.classList.remove("no-scroll");
-  }, 4000);
-});
